b8033da903af395852883ddfa7572b33ed1559a3
BorgesJu <39913185+BorgesJu@users.noreply.github.com>
2024-07-10 14:12:49 +0200

    map dif

---
+++ tilesets/bar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="f11d2b2e-9c31-482b-a215-0e218c421cc1" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="C:/Users/Al3ph/Downloads/f11d2b2e-9c31-482b-a215-0e218c421cc1.webp" width="1024" height="1024"/>
+</tileset>
