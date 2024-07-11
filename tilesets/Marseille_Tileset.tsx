82e70dad356a3f3e325aa8204fffc457f7f70597
BorgesJu <39913185+BorgesJu@users.noreply.github.com>
2024-07-11 11:38:10 +0200

    embed tilset

--- tilesets/Marseille_Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Marseille_Tileset" tilewidth="32" tileheight="32" tilecount="32" columns="8">
- <image source="Marseille_Tileset.png" width="256" height="128"/>
-</tileset>
